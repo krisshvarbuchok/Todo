f38d250158ef83f4bea6fe754fad015483a871f2
Kristina Shvarbukova <shvarbuchok@gmail.com>
2024-07-20 17:20:20 +0300

    withStyleForButton

--- src/components/Todo/AddTask.jsx
+++ src/components/Todo/AddTask.jsx
@@ -1,32 +1,42 @@
-import { Button, Input } from 'antd';
+import { Button, Input, ConfigProvider } from 'antd';
 
-const AddTask = ({logger, task, setTask, setList, setNewTask}) => {
-    const handleClick = ()=>{
-      if(!!task.trim()){
-          setNewTask(task);
-          logger(task);
-          setList((prevItems) => [...prevItems, {task:task, id: crypto.randomUUID()}])
-          setTask('')
-        }
+const AddTask = ({ logger, task, setTask, setList, setNewTask }) => {
+  const handleClick = () => {
+    if (!!task.trim()) {
+      setNewTask(task);
+      logger(task);
+      setList((prevItems) => [...prevItems, { task: task, id: crypto.randomUUID() }])
+      setTask('')
     }
-    const handleKeyDown = (event)=>{
-      if(event.key === 'Enter' && task.trim()){
-        setNewTask(task);
-        logger(task);
-        setList((prevItems) => [...prevItems, {task:task, id: crypto.randomUUID()}])
-        setTask('')
-      }
+  }
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && task.trim()) {
+      setNewTask(task);
+      logger(task);
+      setList((prevItems) => [...prevItems, { task: task, id: crypto.randomUUID() }])
+      setTask('')
     }
-      
-    return (
-        <>
-            <div className='input-with-button'>
-                <Input placeholder="What is the task today?" value={task} onChange={(e) => setTask(e.target.value)} 
-                onKeyDown={handleKeyDown} />
-                <Button type="primary" className='button-add-task' onClick={handleClick}>Add task</Button>
-                
-            </div>
-        </>
-    )
+  }
+
+  return (
+    <>
+      <div className='input-with-button'>
+        <Input placeholder="What is the task today?" value={task} onChange={(e) => setTask(e.target.value)}
+          onKeyDown={handleKeyDown} />
+        <ConfigProvider
+          theme={{
+            components: {
+              Button: {
+                colorPrimaryActive: '#b37feb',
+                colorPrimaryHover: '#722ed1',
+              },
+            },
+          }}
+        >
+          <Button type="primary" className='button-add-task' onClick={handleClick}>Add task</Button>
+        </ConfigProvider>
+      </div>
+    </>
+  )
 }
 export default AddTask;
